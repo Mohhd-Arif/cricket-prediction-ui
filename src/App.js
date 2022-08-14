7e9546fe7c4cfb1a1acc1baabbc6d786294c2dcb
Mohhd-Arif <arifali123454150@gmail.com>
2022-08-15 02:12:45 +0530

    added toast for error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Login from './components/login'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <main className='App'>
--- src/components/login.js
+++ src/components/login.js
@@ -1,42 +1,75 @@
 import { useRef, useState, useEffect } from 'react'
 import userService from '../service/userService'
-
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 const Login = () => {
-    const [usercreds,setUsercreds] = useState({
-        email:"",
-        password:""
+    const [usercreds, setUsercreds] = useState({
+        email: "",
+        password: ""
     });
-    let [showCred,setShowCred] = useState(false);
-    
-    const credHandler = (e)=>{
-        setUsercreds(creds=>({...creds,[e.target.name]:e.target.value}))
+    let [showCred, setShowCred] = useState(false);
+
+    const credHandler = (e) => {
+        setUsercreds(creds => ({ ...creds, [e.target.name]: e.target.value }))
     }
 
-    const loginHandler = (event) =>{
+    const loginHandler = (event) => {
         event.preventDefault()
-        userService(usercreds).then(resp=>{
-            localStorage.setItem("token",resp.data.token);
+        userService(usercreds).then(resp => {
+            localStorage.setItem("token", resp.data.token);
             console.log(localStorage.getItem("token"));
-        }).catch(err=>console.log(err))
+        }).catch(err => console.log(err))
         setShowCred(!showCred)
+    }
+
+
+    function StackingExample(msg) {
+        const [show, setShow] = useState(false);
+
+        return (
+            <Row>
+                <Col xs={6}>
+                    <ToastContainer className="p-3" position="top-end">
+                        <Toast onClose={() => setShow(false)} show={show} >
+                            <Toast.Header>
+                                <img
+                                    src="../../public/logo192.png"
+                                    className="rounded me-10"
+                                    alt=""
+                                />
+                                <strong className="me-auto">Cricket Prediction</strong>
+                                <small>11 mins ago</small>
+                            </Toast.Header>
+                            <Toast.Body>{msg}</Toast.Body>
+                        </Toast>
+                    </ToastContainer>
+                </Col>
+                <Col xs={6}>
+                    <Button onClick={() => setShow(true)}>Show Toast</Button>
+                </Col>
+            </Row>
+        );
     }
 
     return (
         <div className="wrapper">
             <form className="login">
                 <p className="title">Log in</p>
-                <input type="text" name="email" onChange={credHandler} placeholder="Email"/>
+                <input type="text" name="email" onChange={credHandler} placeholder="Email" />
                 <i className="fa fa-user"></i>
                 <input type="password" name="password" onChange={credHandler} placeholder="Password" />
                 <i className="fa fa-key"></i>
-                <br/>
+                <br />
                 <button onClick={loginHandler}>
                     <i className="spinner"></i>
                     <span className="state">Log in</span>
                 </button>
                 {showCred && <p>Hi {usercreds?.email.split('@')[0].toUpperCase()} !!</p>}
             </form>
-            
+            {StackingExample()}
         </div>
     )
 }
